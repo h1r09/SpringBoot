219304d1e6ff597b2e637325c9229beb6cb3df1c
HUNOR MORICZI <hm586@inlumine.ual.es>
2022-04-09 17:35:44 +0200

    HeroController

--- src/main/java/com/example/demo/repository/HeroRepository.java
+++ src/main/java/com/example/demo/repository/HeroRepository.java
@@ -1,11 +1,20 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import com.example.demo.entity.Hero;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+
+
+@CrossOrigin(origins = "", allowedHeaders = "")
 @RepositoryRestResource
 public interface HeroRepository extends CrudRepository<Hero, Long> {
+    List<Hero> findAll();
+    List<Hero> findByName(@Param("name") String name);
 
 }
